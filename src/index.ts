749c4efae5a3fcf3d1bc41c33273e0cba929ce24
yehor-akunishnikov <faiteforever@gmail.com>
2025-01-10 01:21:16 +0200

    Feature: - Helmet - Rate-limiting

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,27 @@ import {StatusCodes} from "http-status-codes";
 import bodyParser from "body-parser";
 import passport from "passport";
 import express from "express";
+import helmet from "helmet";
 
 import {handleGlobalError} from "@common/middlewares";
 import {configurePassport} from "@config/passport";
 import {HttpError} from "@common/errors";
 import {userRouter} from "@user-feature";
 import {authRouter} from "@auth-feature";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
+app.use(helmet());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers.
+}));
 configurePassport(passport);
 
 app.use("/auth", authRouter);
